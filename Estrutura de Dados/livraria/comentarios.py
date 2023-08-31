9b29c65ea2ff5a4e0a3813beedc96b12d355fcad
Leandro Wanderley <devleandromcz@gmail.com>
2023-08-31 18:37:09 -0300

    refactor.alter linkedlist.py

---
+++ Estrutura de Dados/livraria/comentarios.py
@@ -0,0 +1,9 @@
+'''
+Explicando comentários
+
+#! Usando "!" significa que o comentário é importante, ou que precisa ser revisado
+#? Usando "?" significa que o comentário é uma dúvida
+#* Usando "*" significa que o comentário é uma observação
+#TODO Usando "TODO" significa que o comentário é uma tarefa a ser feita
+#// Usando "//" significa que o comentário é uma anotação ja feita
+'''
--- Estrutura de Dados/livraria/linkedlist.py
+++ Estrutura de Dados/livraria/linkedlist.py
@@ -1,6 +1,12 @@
+'''
+#  Função para comprar livro ✅
+#  Função para remover Nó ✅
+#  Emprestar livro ✅
+#? Não lembro mais qual função era pra implementar 😶‍🌫️
+'''
+
 from classes.node import Node
 from classes.livro import Livro
-
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -63,10 +69,14 @@ class LinkedList:
 
     def emprestar(self, livro):
         new_node = self.find_node_livro(livro)
+        if not new_node:
+            return
         if new_node and new_node.value.livros_local > 0:
             new_node.value.livros_local -= 1
             new_node.value.livros_emprestado += 1
+        
         '''
+        #* Função para emprestar livro antes
         new_node = self.head
         for i in range(self.lenght):
             if new_node.value.livro.autor == livro.autor and new_node.value.livro.titulo == livro.titulo and new_node.value.livro.edicao == livro.edicao and new_node.value.livro.editora == livro.editora and new_node.value.livro.ibms == livro.ibms:
@@ -89,10 +99,3 @@ x = Livro('Livro 2', 'Sla 2', 2000, 'Nome 2', 600)
 my_linked_list.emprestar(x)
 
 my_linked_list.print_list()
-
-'''
-#  Função para comprar livro ✅
-#  Função para remover Nó ✅
-#  Emprestar livro ✅
-#! Não lembro mais qual função era pra implementar 😶‍🌫️ ❌
-'''
